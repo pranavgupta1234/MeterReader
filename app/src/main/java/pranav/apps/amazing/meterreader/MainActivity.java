36e82da9c1e1aea5bdda512ddac43939b122638c
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-05-03 19:26:04 +0530

    MeterReader project set up

---
+++ app/src/main/java/pranav/apps/amazing/meterreader/MainActivity.java
@@ -0,0 +1,13 @@
+package pranav.apps.amazing.meterreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
