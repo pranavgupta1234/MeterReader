6f597a5edb901604a2993c601e38f1abda531253
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-06-09 23:22:11 +0530

    login activity created

---
+++ app/src/main/java/pranav/apps/amazing/meterreader/LoginActivity.java
@@ -0,0 +1,37 @@
+package pranav.apps.amazing.meterreader;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.name) EditText name;
+    @BindView(R.id.password) EditText password;
+    @BindView(R.id.login) Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+        ButterKnife.bind(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/pranav/apps/amazing/meterreader/MainActivity.java
+++ app/src/main/java/pranav/apps/amazing/meterreader/MainActivity.java
@@ -1,12 +1,17 @@
 package pranav.apps.amazing.meterreader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pranav Gupta on 6/9/2017.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
